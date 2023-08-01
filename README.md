7a9fb130bc923a4a294597c05bb51e656056049a
devthauan <thauansilva05@hotmail.com>
2023-07-31 23:21:31 -0300

    Enhancements

--- README.md
+++ README.md
@@ -2,11 +2,12 @@
 
 ### This is an assignment of my MBA in Artificial Intelligence.
 ### This is a simple program that uses the library speech_recognition from python to capture the audio from your microphone and converting to text.
-### From this text was implemented a simple calculator that acepts sum and subtraction only and with single digit numbers.
+### From this text was implemented a simple calculator that acepts sum and subtraction only.
 ### The requirements from this project was:
 - Create a program that can listen to your voice and write what it listen
 - Use this program to create a calculator
-- If a person says "one plus two" it should recgnize the numbers and operation and return "1 + 1 = 2"
+- If a person says "one plus two" it should recognize the numbers and operation and return "1 + 1 = 2"
 - If a person says "three minus two" it should recognize the numbers and operation and return "3 - 2 = 1"
-- It should work only for sum and subtraction with any single digit number
+- It should work only for sum and subtraction
 - If the operation division or multiplication is informed it should return a string saying it only works with sum and subtraction
+- If a diferent entry is informed it should say that is and invalid entry
--- main.py
+++ main.py
@@ -1,89 +1,64 @@
-#Esse programa deve ser executado no PyCharm
 import speech_recognition as sr
 
-def obter_audio(microfone):
+def get_audio(microphone):
+    # Select and listen to the default microphone
     with sr.Microphone() as source:
-        microfone.adjust_for_ambient_noise(source)
-        print("Diga alguma coisa: ")
-        audio = microfone.listen(source)
+        microphone.adjust_for_ambient_noise(source)
+        print("Say something: ")
+        audio = microphone.listen(source)
     return audio
 
-def transformar_audio_em_texto(microfone, audio):
+def speach_to_text(microphone, audio):
+    # Transforms audio into text
     try:
-        frase = microfone.recognize_google(audio, language='pt-BR')
-        return frase
+        phrase = microphone.recognize_google(audio, language='pt-BR') # Change language if needed *(using portuguese)
+        return phrase
     except:
-        print("Não entendi")
+        print("Didn't understand.")
 
 
-def escutar_microfone():
-    microfone = sr.Recognizer()
-    audio = obter_audio(microfone)
-    texto =  transformar_audio_em_texto(microfone, audio)
+def listen_microphone():
+    # Get audio and converts to text
+    microphone = sr.Recognizer()
+    audio = get_audio(microphone)
+    texto =  speach_to_text(microphone, audio)
     return texto
 
 
-def find_operands(text, operator_text = "divida", operator_simble = "/"):
-    try:
-        index_operator = text.index(operator_simble)
-    except:
-        index_operator = text.index(operator_text)
-    return [text[index_operator - 1] , text[index_operator + 1]]
+def find_operands(text,  operator_simble):
+    # Get the position of the operator and get values before and after it
+    index_operator = text.index(operator_simble)
+    return [text[index_operator - 1] , text[index_operator + 1]]    
 
-
-
-number_dictionary = {"um":1, "dois":2,"três":3, "quatro":4, "cinco":5, "seis":6, "sete":7, "oito":8, "nove":9}
 
 def text_to_number(first_operand, second_operand):
-    # Transform text to numbers if needed
-    try:
-        first_operand = int(number_dictionary[first_operand])
-        second_operand = int(number_dictionary[second_operand])
-    except:
-        pass
-    # Transform string to int
+    # Transform string operators to int
     try:
         return int(first_operand), int(second_operand)
     except:
-        print("A entrada dos dados não é válida para o algoritmo")
+        print("Invalid entry!")
+
+def get_transformed_operands(splited_text):
+    # Finds and converts operands to integer
+    first_operand, second_operand = find_operands(splited_text, splited_text[1])
+    first_operand, second_operand = text_to_number(first_operand, second_operand)
+    return first_operand, second_operand
+        
 
 if __name__ == '__main__':
-    texto = escutar_microfone()
-    print("A frase que você disse foi: " + texto)
-    split_texto = texto.split(" ")   
-    if("+" in split_texto or "mais" in split_texto):
-        first_operand, second_operand = find_operands(split_texto, "+", "mais")
-        first_operand, second_operand = text_to_number(first_operand, second_operand)
-        print(f"Sua soma é: {first_operand} + {second_operand} = {first_operand + second_operand}")
-    elif("-" in split_texto or "menos" in split_texto):
-        first_operand, second_operand = find_operands(split_texto, "-", "menos")
-        first_operand, second_operand = text_to_number(first_operand, second_operand)
-        print(f"Sua subtração é: {first_operand} - {second_operand} =  {first_operand - second_operand}")
-    elif("x" in split_texto or "vezes" in split_texto):
-        print("O algoritmo trata apenas as operações de Adição e Subtração")
-        #first_operand, second_operand = find_operands(split_texto, "x", "vezes")
-        #first_operand, second_operand = text_to_number(first_operand, second_operand)
-        #print(f"Sua multiplicação é: {first_operand} * {second_operand} =  {first_operand * second_operand}")
+    text = listen_microphone()
+    print("Said phrase: " + text)
+    splited_text = text.split(" ") 
+
+    # Simple calculator logic
+    if("+" in splited_text):
+        first_operand, second_operand = get_transformed_operands(splited_text)
+        print(f"Your sum is: {first_operand} + {second_operand} = {first_operand + second_operand}")
+    elif("-" in splited_text or "menos" in splited_text):
+        first_operand, second_operand = get_transformed_operands(splited_text)
+        print(f"Your subtraction is: {first_operand} - {second_operand} =  {first_operand - second_operand}")
+    elif("x" in splited_text or "/" in splited_text):
+        print("Not supported operations.")
+
     else:
-        print("A entrada dos dados não é válida para o algoritmo")
-
-
-# =============================================================================
-# Saídas
-# =============================================================================
-# =============================== #1# ========================================#
-# Diga alguma coisa: 
-# A frase que você disse foi: 1 + 2
-# Sua soma é: 1 + 2 = 3
-# =============================== #2# ========================================#
-# Diga alguma coisa: 
-# A frase que você disse foi: 3 - 2
-# Sua subtração é: 3 - 2 =  1
-# =============================== #3# ========================================#
-# Diga alguma coisa: 
-# A frase que você disse foi: três vezes dois
-# O algoritmo trata apenas as operações de Adição e Subtração
-# =============================== #4# ========================================#
-# Diga alguma coisa: 
-# A frase que você disse foi: isso é um teste
-# A entrada dos dados não é válida para o algoritmo
+        print("Invalid entry!")
